77e775b1173246cc32dd077e96a0f1de2bac1b10
carlesplusplus <carles.fito.negre@gmail.com>
2022-08-21 10:31:42 +0200

    Docker compose fix, server interal port matching localhost at 3001.

--- server/app.js
+++ server/app.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3001
 
 app.get('/', (req, res) => {
-  res.send('This is gonna be great')
+  res.send('This is gonna be great');
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 })
